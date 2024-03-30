35db47ea87481333286ac43d76218ee596e99846
DevStrikerTech <kcobra557@gmail.com>
2024-03-30 08:28:34 +0000

    added signal with testcases

--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -1,16 +1,15 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    """_summary_
+    """
+    Creates user model with required data fields.
 
     Args:
-        AbstractUser (_type_): _description_
-
-    Returns:
-        _type_: _description_
+        AbstractUser (obj): inherits from built-in user model
     """
-    username = models.CharField(unique=True, max_length=100)
+    username = models.CharField(unique=True, max_length=50)
     email = models.EmailField(unique=True)
     full_name = models.CharField(unique=True, max_length=100)
     otp = models.CharField(max_length=100, null=True, blank=True)
@@ -20,16 +19,9 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['username']
 
     def __str__(self):
-        """_summary_
-
-        Returns:
-            _type_: _description_
-        """
         return self.email
     
     def save(self, *args, **kwargs):
-        """_summary_
-        """
         email_username, full_name = self.email.split("@")
         if self.full_name == "" or self.full_name == None:
             self.full_name == email_username
@@ -38,13 +30,11 @@ class User(AbstractUser):
         super(User, self).save(*args, **kwargs)
    
 class Profile(models.Model):
-    """_summary_
-
+    """
+    Creates a profile based on given user.
+    
     Args:
-        models (_type_): _description_
-
-    Returns:
-        _type_: _description_
+        models (obj): uses the custom user model
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.FileField(upload_to="user_folder", default="default-user.jpg", null=True, blank=True)
@@ -54,11 +44,6 @@ class Profile(models.Model):
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        """_summary_
-
-        Returns:
-            _type_: _description_
-        """
         if self.full_name:
             return str(self.full_name)
         else:
@@ -66,10 +51,31 @@ class Profile(models.Model):
         
     
     def save(self, *args, **kwargs):
-        """_summary_
-        """
         if self.full_name == "" or self.full_name == None:
             self.full_name == self.user.username
         super(Profile, self).save(*args, **kwargs)
 
+def create_user_profile(sender, instance, created, **kwargs):
+    """
+    Creates a user profile
 
+    Args:
+        sender (str): Specifies a particular sender to receive signals from.
+        instance (str): user data
+        created (str): timestamp
+    """
+    if created:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    """
+    Saves user profile
+
+    Args:
+        sender (str): Specifies a particular sender to receive signals from.
+        instance (str): user data
+    """
+    instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
--- backend/userauths/tests.py
+++ backend/userauths/tests.py
@@ -1,21 +1,45 @@
+from unittest.mock import Mock
 from django.test import TestCase
-from userauths.models import User, Profile
+from userauths.models import User, Profile, create_user_profile, save_user_profile
 
 class UserAuthsModelTest(TestCase):
     def test_create_user_with_valid_details(self):
-        user = User(email="test@example.com", username="testuser", full_name="Test User")
+        user = User(email="test@example.com", username="test", full_name="Test User")
         user.save()
         assert user.email == "test@example.com"
-        assert user.username == "testuser"
+        assert user.username == "test"
         assert user.full_name == "Test User"
 
     def test_create_profile_with_required_fields(self):
         user = User.objects.create(email="test@example.com", username="test")
-        profile = Profile.objects.create(user=user, full_name="John Doe")
-    
+
+        try:
+            profile = Profile.objects.get(user=user)
+        except Profile.DoesNotExist:
+            profile = Profile.objects.create(user=user, full_name="John Doe")
+
         assert profile.user == user
-        assert profile.full_name == "John Doe"
+        assert profile.full_name == ""
         assert profile.image == "default-user.jpg"
-        assert profile.country == None
-        assert profile.about == None
-        assert profile.date != None
+        assert profile.country is None
+        assert profile.about is None
+        assert profile.date is not None
+        
+    def test_create_user_profile_success(self):
+        user = User.objects.create(username="test", email="test@example.com")
+
+        try:
+            profile = Profile.objects.get(user=user)
+        except Profile.DoesNotExist:
+            profile = Profile.objects.create(user=user)
+
+        assert profile is not None
+        
+    def test_saves_user_profile_with_valid_arguments(self):
+        sender = "Sender"
+        instance = Mock()
+        instance.profile = Mock()
+    
+        save_user_profile(sender, instance)
+    
+        instance.profile.save.assert_called_once()
