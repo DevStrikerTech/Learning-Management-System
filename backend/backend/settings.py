989c9872ca41bec1c94443f7fa721016a1de8eab
DevStrikerTech <kcobra557@gmail.com>
2024-04-03 08:54:33 +0100

    added swagger ui

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,6 +118,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
     "anymail",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,8 +19,33 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/v1/", include("api.urls"))]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="LMS Backend APIs",
+        default_version="v1.0.0",
+        description="This is the API documentation for LMS project APIs",
+        terms_of_service="https://github.com/DevStrikerTech/Learning-Management-System/blob/dev/LICENSE",
+        contact=openapi.Contact(email="@DevStrikerTech"),
+        license=openapi.License(name="GPL-3.0 License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("api.urls")),
+]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
