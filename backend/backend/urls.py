96dfa98edaa4fa96a84acf50caf17bc4250bbc81
DevStrikerTech <kcobra557@gmail.com>
2024-04-03 07:21:57 +0100

    added mail services

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [path("admin/", admin.site.urls), path("api/v1/", include("api.urls"))]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
